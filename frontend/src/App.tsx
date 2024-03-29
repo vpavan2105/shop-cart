e0bf388e3e5f7eb6e30738c478488443d538feed
Indrani-Paul <paul.indrani999@gmail.com>
2024-03-29 16:23:20 +0530

    Post rerequest to order for new order

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-
+import { OrderList } from './components/OrderList'
 import './App.css'
 // import { Payments } from './components/Payments'
 function App() {
 
   return (
     <>
+    <OrderList></OrderList>
     </>
   )
 }
--- frontend/src/components/OrderList.tsx
+++ frontend/src/components/OrderList.tsx
@@ -1,50 +1,125 @@
-import { ReactElement, useRef } from "react";
-import {FormControl,FormLabel,Input,Button } from '@chakra-ui/react'
+import { ReactElement, useEffect, useRef, useState } from "react";
+import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
-
-
-export function OrderList():ReactElement{
-    const name = useRef<HTMLInputElement>(null);
-    const address = useRef<HTMLInputElement>(null);
-    const pincode = useRef<HTMLInputElement>(null);
-    const city = useRef<HTMLInputElement>(null);
-    const state = useRef<HTMLInputElement>(null);
-    const country = useRef<HTMLInputElement>(null);
-    const handleOrder=()=>{
-        // async function postRequest() {
-        //     let res=await fetch('http://localhost:3001/orders', {method:'POST', body : JSON.stringify({
-
-        //     })})
-        // }
-        console.log(name.current?.value);
-        console.log(address.current?.value);
-        console.log(pincode.current?.value);
-        console.log(city.current?.value);
-        console.log(state.current?.value);
-        console.log(country.current?.value);
-         async function postRequest() {
-            let res=await fetch('http://localhost:3001/orders', {method:'POST', body : JSON.stringify({
-                userid:'1',
-                name:name.current?.value,
-                address:[address.current?.value, pincode.current?.value, city.current?.value, state.current?.value, country.current?.value]
-            }), headers:{'Content-type':'application'}})
-        }
-    }
-
-    return <><FormControl>
-    <FormLabel>Name</FormLabel>
-    <Input placeholder='Name' ref={name} />
-    <FormLabel>Address</FormLabel>
-    <Input placeholder='Address' ref={address}/>
-    <FormLabel>Pincode</FormLabel>
-    <Input placeholder='Pincode' ref={pincode}/>
-    <FormLabel>City</FormLabel>
-    <Input placeholder='City' ref={city}/>
-    <FormLabel>State</FormLabel>
-    <Input placeholder='State' ref={state}/>
-    <FormLabel>Country</FormLabel>
-    <Input placeholder='Country' ref={country}/>
-    <Button bg="yellow" color='black' variant='outline' onClick={handleOrder}>Place Order</Button>
-  </FormControl></>
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+  };
+  quantity: number;
 }
+//component
+export function OrderList(): ReactElement {
+  const [cartProduct, setCartProduct] = useState<Product[] | null>(null);
+  const [totalPrice, setTotalPrice] = useState<number>(0);
+  // get userid from local storage
+  let user_id = 1; //here hardcoded
+  const name = useRef<HTMLInputElement>(null);
+  const address = useRef<HTMLInputElement>(null);
+  const pincode = useRef<HTMLInputElement>(null);
+  const city = useRef<HTMLInputElement>(null);
+  const state = useRef<HTMLInputElement>(null);
+  const country = useRef<HTMLInputElement>(null);
+
+  // total price calculation
+  useEffect(() => {
+    let total = 0;
+    if (cartProduct) {
+      for (const product of cartProduct) {
+        total += product.price * product.quantity;
+      }
+      total = parseFloat(total.toFixed(2));
+      setTotalPrice(total);
+    }
+  }, [cartProduct]);
+
+  useEffect(() => {
+    // Calling postData function after totalPrice calculation
+    if (cartProduct !== null) {
+      postData(cartProduct);
+    }
+  }, [totalPrice]);
+
+
+  //handleorder function will call when userv will click on the Place order button
+  const handleOrder = () => {
+
+    //getCartProductData function for fetching cartdetails of the user and extract the product array
+    async function getCartProductData() {
+      try {
+        let res = await fetch(`http://localhost:3001/carts/${user_id}`);
+        let data = await res.json();
+        setCartProduct(data.products);  //extracting the product array
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getCartProductData();
+  };
+
+//Send POST request to the server to create a new order
+  async function postData(cartProduct: Product[]) {
+    try {
+      const orderId = uuidv4(); //generate a UUID for the order ID
+      const currentDate = new Date(); // get the current date and time
+      const formattedDate = currentDate.toLocaleString(); // Convert the current date and time to a string representation
+      console.log(currentDate);
+      let res = await fetch("http://localhost:3001/orders", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userid: "1",
+          name: name.current?.value,
+          address: [
+            address.current?.value,
+            pincode.current?.value,
+            city.current?.value,
+            state.current?.value,
+            country.current?.value,
+          ],
+          date: formattedDate,
+          allProducts: cartProduct,
+          totalAmount: totalPrice,
+        }),
+      });
+    } catch (error) {
+      console.error("Error creating order:", error);
+    }
+  }
+  return (
+    <>
+      <FormControl>
+        <FormLabel>Name</FormLabel>
+        <Input placeholder="Name" ref={name} />
+        <FormLabel>Address</FormLabel>
+        <Input placeholder="Address" ref={address} />
+        <FormLabel>Pincode</FormLabel>
+        <Input placeholder="Pincode" ref={pincode} />
+        <FormLabel>City</FormLabel>
+        <Input placeholder="City" ref={city} />
+        <FormLabel>State</FormLabel>
+        <Input placeholder="State" ref={state} />
+        <FormLabel>Country</FormLabel>
+        <Input placeholder="Country" ref={country} />
+        <Button
+          bg="yellow"
+          color="black"
+          variant="outline"
+          onClick={handleOrder}
+        >
+          Place Order
+        </Button>
+      </FormControl>
+    </>
+  );
+}
+
 
