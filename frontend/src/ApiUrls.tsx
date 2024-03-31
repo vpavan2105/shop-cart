69ee68cf65c6463a8d3b5e937e26d254ae90eee0
pavan kumar <vpavankumar2105@gmail.com>
2024-03-31 21:50:49 +0530

    created api and updated code

---
+++ frontend/src/ApiUrls.tsx
@@ -0,0 +1,5 @@
+export const BaseUrl = `https://shop-cart-x0xf.onrender.com/`;
+export const ProductUrl = `https://shop-cart-x0xf.onrender.com/products`;
+export const CartUrl = `https://shop-cart-x0xf.onrender.com/carts`;
+export const UserUrl = `https://shop-cart-x0xf.onrender.com/users`
+export const OrderUrl = `https://shop-cart-x0xf.onrender.com/orders`;
