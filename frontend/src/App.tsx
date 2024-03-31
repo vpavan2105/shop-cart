8b4efe3532126534cbbe1d6cc69799262e996324
Indrani-Paul <paul.indrani999@gmail.com>
2024-03-31 22:03:58 +0530

    changes done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,48 @@
 
-// import { useState } from 'react'
+// // import { useState } from 'react'
+// import './App.css'
+// import { Home } from './pages/Home'
+// // import AllRoutes from './components/AllRoutes/AllRoutes'
+// // import NavBar from './components/AllRoutes/NavBar'
+// // import AdminNavBar from './components/AllRoutes/AdminNavBar'
+
+// function App() {
+
+
+//   return (
+//     <>
+//     {/* {isAuth? <AdminNavBar/> : <NavBar/> }
+//     <AllRoutes/> */}
+   
+//     <Home/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
 import './App.css'
-import { Home } from './pages/Home'
-// import AllRoutes from './components/AllRoutes/AllRoutes'
-// import NavBar from './components/AllRoutes/NavBar'
-// import AdminNavBar from './components/AllRoutes/AdminNavBar'
-
+import CartList from './components/CartList'
+// import { OrderList } from './components/OrderList'
+// import { OrderDisplay } from './components/OrderDisplay'
+// import ProductList from './components/Products/ProductList'
+// import SingleProductPage from './components/Products/SingleProductCard'
 function App() {
-
-
   return (
     <>
-    {/* {isAuth? <AdminNavBar/> : <NavBar/> }
-    <AllRoutes/> */}
-    <Home/>
+   <CartList/>
+   {/* <OrderList></OrderList> */}
+   {/* <OrderDisplay></OrderDisplay> */}
+   {/* <ProductList /> */}
+   {/* <SingleProductPage /> */}
+  
     </>
   )
 }
 
 export default App
-
-
-
-
 
