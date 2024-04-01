47131e755ee1be68350c78a6a22c30be5a30416a
PrasadPawar2729 <prasadpawarpp2002@gmail.com>
2024-04-01 13:08:56 +0530

    day6

---
+++ frontend/src/pages/Footer.tsx
@@ -0,0 +1,115 @@
+ import "../home.css";
+
+
+function Footer(){
+    return(
+        <>
+          <div className="footer">
+    <div className="nav-1 nav">
+      <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63e86ab4c21faa7bc0bd90dd_Logo.svg" alt="" />
+      <h3>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</h3>
+   
+   <h2>Accepted Payments</h2>
+   
+   <div className="img-boxe">
+    <div className="img-boxes">
+      <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb1ce8816711ebecac46d8_stripe.png" alt="" />
+    </div>
+    <div className="img-boxes">
+      <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb1ce82d440b7ab84a993f_visa.png" alt="" />
+    </div>
+    <div className="img-boxes">
+    <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb1ce8f032504012a5896b_Mastercard.png" alt="" />
+    </div>
+    <div className="img-boxes">
+    <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63e8c4e48b497e6ce846b7ff_Amazon.png" alt="" />
+    </div>
+    <div className="img-boxes">
+<img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb1f054e419e42aca4a9a2_Klarna.png" alt="" />
+    </div>
+    <div className="img-boxes">
+  <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb1ce7c4510cf9a55828a0_PayPal.png" alt="" />
+    </div>
+    <div className="img-boxes">
+<img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63e8c4e4707380264b25e680_ApplePay.png" alt="" />
+    </div>
+    <div className="img-boxes">
+ <img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb1f55dc68c5ee83d0cbf8_GooglePay.png" alt="" />
+    </div>
+   </div>
+   
+    </div>
+
+     <div className="nav-2 nav footer1">
+     <ul >
+     <li><a href="#department"><h2>Department</h2></a></li>
+    <li><a href="#fashion">Fashion</a></li>
+    <li><a href="#education">Education Product</a></li>
+    <li><a href="#frozen-food">Frozen Food</a></li>
+    <li><a href="#beverages">Beverages</a></li>
+    <li><a href="#organic-grocery">Organic Grocery</a></li>
+    <li><a href="#office-supplies">Office Supplies</a></li>
+    <li><a href="#beauty-products">Beauty Products</a></li>
+    <li><a href="#books">Books</a></li>
+    <li><a href="#electronics">Electronics & Gadget</a></li>
+    <li><a href="#travel-accessories">Travel Accessories</a></li>
+    <li><a href="#fitness">Fitness</a></li>
+    <li><a href="#sneakers">Sneakers</a></li>
+    <li><a href="#toys">Toys</a></li>
+    <li><a href="#furniture">Furniture</a></li>
+  </ul>
+  
+
+
+    </div>
+    <div className="nav-3 nav footer1">
+    <ul>
+    <li><a href="#aboutus"><h2>About us</h2></a></li>
+    <li><a href="#about-shopcart">About Shopcart</a></li>
+    <li><a href="#careers">Careers</a></li>
+    <li><a href="#news-blog">News & Blog</a></li>
+    <li><a href="#help">Help</a></li>
+    <li><a href="#press-center">Press Center</a></li>
+    <li><a href="#shop-location">Shop By Location</a></li>
+    <li><a href="#shopcart-brands">Shopcart Brands</a></li>
+    <li><a href="#affiliate-partners">Affiliate & Partners</a></li>
+    <li><a href="#ideas-guides">Ideas & Guides</a></li>
+  </ul>
+    </div>
+    <div className="nav-4 nav footer1">
+    <ul>
+    <li><a href="#services"><h2>services</h2></a></li>
+    <li><a href="#gift-card">Gift Card</a></li>
+    <li><a href="#mobile-app">Mobile App</a></li>
+    <li><a href="#shipping-delivery">Shipping & Delivery</a></li>
+    <li><a href="#order-pickup">Order Pickup</a></li>
+    <li><a href="#account-signup">Account Signup</a></li>
+  </ul>
+    </div>
+    <div className="nav-5 nav footer1">
+    <ul>
+    <li><a href="#help"><h2>Help</h2></a></li>
+    <li><a href="#shopcart-help">Shopcart Help</a></li>
+    <li><a href="#returns">Returns</a></li>
+    <li><a href="#track-orders">Track Orders</a></li>
+    <li><a href="#contact-us">Contact Us</a></li>
+    <li><a href="#feedback">Feedback</a></li>
+    <li><a href="#security-fraud">Security & Fraud</a></li>
+  </ul>
+    </div> 
+   </div>
+  <hr /> <hr />
+
+<div className="fo-logo">
+  <h2><span><img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb0ed6e927bdf5bc4309e0_briefcase.svg" alt="" /></span>   Become Seller</h2>
+ <h2><span><img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb0ed6c4510c256356f4cd_gift.svg" alt="" /></span>Gift Cards</h2>
+<h2><span><img src="https://assets-global.website-files.com/63e857eaeaf853471d5335ff/63eb0ed6ae57fd74e0402aa4_help-circle.svg" alt="" /></span>help Center</h2>
+<h2><a href="#">All Right reserved by Musemind ui/ux design agency | 2024</a></h2>
+</div>
+        
+        </>
+    )
+}
+
+
+export {Footer}
